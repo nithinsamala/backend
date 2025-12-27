9a3d3c30e2a132df34638abddb38e0f1a185839b
nithinsamala <blkg896@gmail.com>
2025-12-27 17:56:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://fend-ev3y.vercel.app" // ✅ ONLY DOMAIN
+    "https://fend-jpqu.vercel.app/" // ✅ ONLY DOMAIN
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
