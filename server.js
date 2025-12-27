95355fff59925949e71dc3651a97545f8681fba6
nithinsamala <blkg896@gmail.com>
2025-12-27 16:41:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://YOUR-VERCEL-APP.vercel.app" // 🔴 replace with real URL
+    "https://fend-sand.vercel.app"
   ],
   credentials: true
 }));
